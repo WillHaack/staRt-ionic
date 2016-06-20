3fab60a1e8e5621e462d23f3f282c0a193d24402
Sam Tarakajian <starakajian@gmail.com>
2016-06-20 19:22:46 +0200

    Rename alert.js to audioplugin.js

---
+++ audio-plugin/www/audioplugin.js
@@ -0,0 +1,81 @@
+module.exports = {
+  getLPCCoefficients: function(successCallback) {
+    cordova.exec(successCallback,
+                 null, // No failure callback
+                 "AudioPlugin",
+                 "getLPCCoefficients",
+                 null); // No arguments
+  },
+  startRecording: function(accountDescription, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "AudioPlugin",
+      "startRecording",
+      [accountDescription]
+    );
+  },
+  stopRecording: function(successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "AudioPlugin",
+      "stopRecording",
+      null
+    );
+  },
+  isRecording: function(successCallback) {
+    cordova.exec(
+      successCallback,
+      null,
+      "AudioPlugin",
+      "isRecording",
+      null
+    );
+  },
+  recordingsForAccount: function(accountDescription, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "AudioPlugin",
+      "recordingsForAccount",
+      [accountDescription]
+    );
+  },
+  deleteRecording: function(recordingDescription, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "AudioPlugin",
+      "deleteRecording",
+      [recordingDescription]
+    );
+  },
+  deleteAllRecordings: function(successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      "AudioPlugin",
+      "deleteAllRecordings",
+      null
+    );
+  },
+  getLPCOrder: function(successCallback) {
+    cordova.exec(
+      successCallback,
+      null,
+      "AudioPlugin",
+      "getLPCOrder",
+      null
+    );
+  },
+  setLPCOrder: function(newOrder, successCallback) {
+    cordova.exec(
+      successCallback,
+      null,
+      "AudioPlugin",
+      "setLPCOrder",
+      [newOrder]
+    );
+  }
+};
