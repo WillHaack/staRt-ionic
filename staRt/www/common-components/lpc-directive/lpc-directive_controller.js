766759e5611d5a12935c441d34b4133aaf28fa74
Sam Tarakajian <starakajian@gmail.com>
2016-07-12 14:50:11 +0200

    Trying to make our display look more like theirs

--- staRt/www/common-components/lpc-directive/lpc-directive_controller.js
+++ staRt/www/common-components/lpc-directive/lpc-directive_controller.js
@@ -25,9 +25,11 @@ lpcDirective.controller( 'LpcDirectiveController', function( $rootScope, $scope,
 		var running = true;
 		var frequencyScaling = 1.0;
 		var points = [];
+		var peaks = [];
 
 		lpc.coefficentCallback = function(msg) {
 			points = msg.coefficients;
+			peaks = msg.freqPeaks;
 			frequencyScaling = msg.freqScale;
 		};
 
@@ -35,23 +37,39 @@ lpcDirective.controller( 'LpcDirectiveController', function( $rootScope, $scope,
 		};
 
 		lpc.setup = function() {
-			myCanvas = lpc.createCanvas(screen.width, screen.height/2);
+			myCanvas = lpc.createCanvas(screen.width, screen.height/2, lpc.WEBGL);
 			myCanvas.parent(element.children()[0]);
+			var width = screen.width;
+			var height = screen.height/2.0;
+			lpc.ortho(-width, width, -height, height);
 			lpc.frameRate(myFrameRate);
 		};
 
 		lpc.draw = function() {
-			lpc.background('#aaffaa');
-			$scope.getLPCCoefficients(lpc.coefficentCallback);
-			lpc.stroke('#000000');
-			lpc.strokeWeight(3);
+			lpc.background('#00ff00');
+			lpc.line(-1, 0, 1, 1, 0, 1);
+			//$scope.getLPCCoefficients(lpc.coefficentCallback);
+			lpc.strokeWeight(1);
 			lpc.noFill();
-			lpc.beginShape();
+
+			return;
 
       // Not sure exactly how the points are meant to be scaled...
       var scaleFac = -1.0;
 
-      for (var i=0; i<points.length; i++) {
+			// First draw the peaks, so that they'll appear behind
+			lpc.stroke('#0000ff');
+			for (var i=0; i<peaks.length; i++) {
+				var px = peaks[i].X;
+				px = (px * (myCanvas.width/2) + (myCanvas.width/2)) * frequencyScaling;
+				var py = peaks[i].Y * scaleFac;
+				py = py * (myCanvas.height/2) + (myCanvas.height/2);
+				lpc.line(px, myCanvas.height, px, py);
+			}
+
+			lpc.stroke('#00ff00');
+			lpc.beginShape();
+      for (i=0; i<points.length; i++) {
       	var px = i / (points.length) * myCanvas.width * frequencyScaling;
       	var py = points[i] * scaleFac;
       	py = py * (myCanvas.height/2) + (myCanvas.height/2);
