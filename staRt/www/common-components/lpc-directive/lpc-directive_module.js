9c24966d2fe663b49d73cc64578470c155ec6c4d
Ana <agiraldow@gmail.com>
2016-07-26 16:25:16 -0400

    Add binary slider attribute to LPC directive

--- staRt/www/common-components/lpc-directive/lpc-directive_module.js
+++ staRt/www/common-components/lpc-directive/lpc-directive_module.js
@@ -8,6 +8,10 @@ lpcDirective.directive( 'lpcDirective', function()
 
 		restrict: 'E',
 		controller: 'LpcDirectiveController',
+		scope:
+		{
+			slider: '='
+		},
 		templateUrl: 'common-components/lpc-directive/lpc-directive_template.html'
 	};
 } );
