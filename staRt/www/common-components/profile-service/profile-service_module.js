e3cfda1b07ee1e20b1bde23b9fb20aad4d4544cf
Ana <agiraldow@gmail.com>
2016-07-29 11:25:02 -0400

    Create profile service

---
+++ staRt/www/common-components/profile-service/profile-service_module.js
@@ -0,0 +1,44 @@
+var profileService = angular.module('profileService', []);
+
+profileService.factory('ProfileService', function($rootScope, $scope, $localForage)
+{
+
+	return
+	{
+		function getAllProfiles()
+		{
+
+		}
+
+		function deleteAllProfiles()
+		{
+
+		}
+
+		function getCurrentProfile()
+		{
+
+		}
+
+		function setCurrentProfile(profile)
+		{
+
+		}
+
+		function saveProfile(profile)
+		{
+
+		}
+
+		function createProfile(profile)
+		{
+
+		}
+
+		function deleteProfile(profile)
+		{
+
+		}
+	}
+
+});
