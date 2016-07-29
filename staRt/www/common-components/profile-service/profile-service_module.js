c14aa453de0ea4699551b112927cd7d6f8a2a19f
Ana <agiraldow@gmail.com>
2016-07-29 11:39:59 -0400

    Write profileService saveProfile function

--- staRt/www/common-components/profile-service/profile-service_module.js
+++ staRt/www/common-components/profile-service/profile-service_module.js
@@ -7,27 +7,42 @@ profileService.factory('ProfileService', function($rootScope, $scope, $localFora
 	{
 		function getAllProfiles()
 		{
-
+			return $localForage.getItem('profiles');
 		}
 
 		function deleteAllProfiles()
 		{
-
+			return $localForage.setItem('profiles', undefined);
 		}
 
 		function getCurrentProfile()
 		{
-
+			return $localForage.getItem('currentProfile');
 		}
 
 		function setCurrentProfile(profile)
 		{
-
+			return $localForage.setItem('currentProfile', profile);
 		}
 
 		function saveProfile(profile)
 		{
-
+			$localForage.getItem('profiles').then(function(profiles)
+			{
+				var idx = profiles.findIndex(function(el)
+				{
+					return el.uuid == this.uuid;
+				}, profile);
+				if (idx !== -1)
+				{
+					profiles[idx] = profile;
+				}
+				else
+				{
+					alert('Profile doesn\'t exist!');
+				}
+				$localForage.setItem('profiles', profiles);
+			});
 		}
 
 		function createProfile(profile)
