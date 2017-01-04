7361085151c8d3976d324a07fb09d31e22962afe
helencarey <helencarey@nyu.edu>
2017-01-04 14:18:35 -0500

    state save

---
+++ staRt/www/states/root/tutorial/junk.js
@@ -0,0 +1,59 @@
+	syllables.config( function( $stateProvider )
+	{
+		$stateProvider.state( 'root.syllables',
+		{
+			url: 'syllables',
+			views:
+			{
+				'content-view':
+				{
+					templateUrl: 'states/root/syllables/syllables_template.html',
+					controller: 'SyllablesController as syllables'
+				}
+			}
+			// abstract: true,
+		} );
+	} );
+
+} )(  );
+
+
+
+/*
+			.state( 'tutorial-p01', {
+				url: 'tutorial-step1',
+				template: '<h1>Step 1. The Wave</h1>'
+
+				// views:
+				// {
+				// 	'content-view':
+				// 	{
+				// 		templateUrl: 'states/root/tutorial/tutorial_template.html',
+				// 		controller: 'TutorialController as tutorial'
+				// 	}
+				// abstract: true,
+			} )
+
+			.state( 'tutorial-p02', {
+				url: 'tutorial-step2',
+				template: '<h1>Step 2. eee sounds/h1>'
+
+				// views: {
+				// 	// the main template (relatively named)
+				// 	'content-view': {
+				// 		templateUrl: 'states/root/tutorial/tutorial_template.html',
+				// 		controller: 'TutorialController as tutorial'
+				// 	},
+				// 	// the child views will be defined here (absolutely named)
+    //         		'columnOne@step2': { 
+    //         			template: 'Look I am a column!' 
+    //         		},
+    //         		 // for column two, we'll define a separate controller 
+		  //           'columnTwo@step2': { 
+		  //               templateUrl: 'table-data.html',
+		  //               controller: 'scotchController'
+		  //           }
+				// } // end views 
+				// abstract: true,
+			} );
+*/
--- staRt/www/states/root/tutorial/tutorial_module.js
+++ staRt/www/states/root/tutorial/tutorial_module.js
@@ -5,21 +5,44 @@
 	var tutorial = angular.module( 'tutorial',
 	[ ] );
 
-	tutorial.config( function( $stateProvider )
-	{
-		$stateProvider.state( 'root.tutorial',
-		{
-			url: 'tutorial',
-			views:
-			{
-				'content-view':
-				{
+	tutorial.config( function( $stateProvider) {
+		
+		$stateProvider
+		// root . this is the parent of all other views
+		.state('root.tutorial', {
+			url: 'tutorial', // this sets the active left-nav tab to tutorial
+			views: {
+				'content-view': {
 					templateUrl: 'states/root/tutorial/tutorial_template.html',
 					controller: 'TutorialController as tutorial'
 				}
 			}
-			// abstract: true,
-		} );
-	} );
+		}) //end root template
 
+			.state('root.tutorial.p01', {
+				url: 'tutorial', 
+				template: '<h1>Step 1</h1>'
+			}) //end step1 template
+
+			.state('root.tutorial.p02', {
+				url: 'tutorial',
+				template: '<h1>Step 2</h1>'
+			}) //end step2 template
+
+			.state('root.tutorial.p03', {
+				url: 'tutorial',
+				template: '<h1>Step 3</h1>'
+			}) //end step3 template
+
+			.state('root.tutorial.p04', {
+				url: 'tutorial',
+				template: '<h1>Step 4</h1>'
+			}) //end step4 template
+
+			.state('root.tutorial.p05', {
+				url: 'tutorial',
+				template: '<h1>Step 5</h1>'
+			}) //end step5 template
+
+	} ); // end tutorial.config
 } )(  );
