3cc55e5e9804f46fc712a86ff96337bc9d0f13f6
helencarey <helencarey@nyu.edu>
2018-09-19 10:40:36 -0400

    See #243. Ready for review. this commit adds new styles to Resources as an example.

--- staRt/www/states/root/resources/resources_controller.js
+++ staRt/www/states/root/resources/resources_controller.js
@@ -12,13 +12,14 @@
 			configuring: false,
       lpcOrder: 35,
       version: "",
-      platform: ""
-    };
+      platform: "",
+			navTitle: "SLP Resources"
 
-    // cordova.getAppVersion.getVersionNumber().then(function (version) {
-    //   $scope.data.version = `${version}`;
-    //   $scope.data.platform = `${device.platform} ${device.version}`
-    // });
+    };
+    cordova.getAppVersion.getVersionNumber().then(function (version) {
+      $scope.data.version = `${version}`;
+      $scope.data.platform = `${device.platform} ${device.version}`
+    });
 
 		$scope.$on("$ionicView.enter", function() {
 			console.log('view content loaded!');
