1299f9d2a3c9d007e4d42f0999c2f1b87fbdcc26
helencarey <helencarey@nyu.edu>
2018-10-23 15:59:34 -0400

    See #258 #259 #276. Updates to Resources pages.

--- staRt/www/states/root/resources/resources_controller.js
+++ staRt/www/states/root/resources/resources_controller.js
@@ -14,7 +14,6 @@
       version: "",
       platform: "",
 			navTitle: "SLP Resources"
-
     };
      cordova.getAppVersion.getVersionNumber().then(function (version) {
        $scope.data.version = `${version}`;
@@ -73,78 +72,6 @@
 			}
 		};
 
-		// ============================================================
-		// RESOURCES INDEX PAGE CONTENT -------------------------------
-		$scope.resPageData = {
-			downloads: {
-				title: "Downloads",
-				description: "Download resources as PDFs.",
-				items: [
-					{
-						title: "staRt User Manual",
-						order: 0,
-						url: "https://www.dropbox.com/sh/x5m8iz4u6crve6r/AACxiy7yjmz9yH_n8vIscBqca"
-					}
-					/*,{
-						title: "Guide for Starting Pilot Study",
-						order: 0,
-						url: "https://www.dropbox.com/sh/z4uvvnm6wajgve3/AADxCwlmmB6yKa3Ijs4lD26Va"
-					}
-					,{
-						title: "Checklist for Interested Clinicians",
-						order: 0,
-						url: "https://www.dropbox.com/sh/uanqcaau83s5uaa/AABnlD5sca9EmmnptSYyCHF9a"
-					}
-					,{
-						title: "Principal Letter",
-						order: 0,
-						url: "https://www.dropbox.com/sh/dw2l2l4555ucwrj/AAD6JzC3WMfZ4SdexFLWRI4ka"
-					}
-					,{
-						title: "Parent Letter",
-						order: 0,
-						url: "https://www.dropbox.com/sh/41fay3xx5aniw1e/AACV8basbDgEX13Da68y9EJOa"
-					}
-					,{
-						title: "SLP Informational Letter",
-						order: 0,
-						url: "https://www.dropbox.com/sh/pq5p6j0fz03sgjw/AAC-Wy7W9t61ugTfO-tZNEg0a"
-					}
-					,{
-						title: "Clinician Questionnaires",
-						order: 0,
-						url: "https://www.dropbox.com/sh/p79m84qq9kprare/AACmj0X28Xmr6HFPk8I_erOAa"
-					} */
-				]
-			},
-			links: {
-				title: "Links",
-				description: "Opens in your device's browser.",
-				items: [
-					{
-						title: "staRt Video Demo",
-						order: 0,
-						url: "https://www.youtube.com/watch?v=kGFdED4HDcw"
-					}
-					,{
-						title: "Become a Pilot Tester",
-						order: 0,
-						url: "https://wp.nyu.edu/byunlab/projects/start/participate/"
-					}
-					/*,{
-						title: "Pilot Tester Survey",
-						order: 0,
-						url: "https://docs.google.com/forms/d/e/1FAIpQLSewiD8Uxle5WRPh5ArgUwYNQA6lktpjA8-xwSQVqinTW1IQiA/viewform?c=0&w=1"
-					}
-					,{
-						title: "Bug Report",
-						order: 0,
-						url: "https://docs.google.com/forms/d/e/1FAIpQLScfSrv-fbkMg7AaZerT0ssXAbLJ20LD2h-XZQXBFAXbyUkWmw/viewform?c=0&w=1"
-					} */
-				]
-			}
-		};
-		// end res page content ----------------
 
 	});
 
