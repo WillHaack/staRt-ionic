407b8cf8d63f8c50fbef7315736ad13afc129159
helencarey <helencarey@nyu.edu>
2018-10-30 12:54:50 -0400

    updates text on final slide.

---
+++ staRt/www/states/root/tutorial/junk.js
@@ -0,0 +1,6 @@
+"<p>You will practice matching your wave’s <span class='numbBub--yellowMain'>3</span>rd peak to the target line.</p>",
+
+"<p>You or your SLP can move the target further to the left for a more challenging target--but don't go further than about where you see the starfish in this picture.</p>",
+
+"<p class='txtBold'>That’s all there is to it.</p>",
+"<div class='rightRow-spacer'>.</div>"
