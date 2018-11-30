fe216af583c38c8219c0e10dbbc9e3fbec9b8c05
helencarey <helencarey@nyu.edu>
2018-11-30 16:33:11 -0500

    disables all old content in Resources

--- staRt/www/states/root/resources/resources_controller.js
+++ staRt/www/states/root/resources/resources_controller.js
@@ -8,9 +8,9 @@
 	{
 		console.log('ResourcesController here!');
 
+		// configuring: false,
+		// lpcOrder: 35,
 		$scope.data = {
-			configuring: false,
-      lpcOrder: 35,
       version: "",
       platform: "",
 			navTitle: "SLP Resources"
@@ -22,13 +22,16 @@
      });
 
 		$scope.$on("$ionicView.enter", function() {
-			console.log('view content loaded!');
+			console.log('resources content loaded!');
+			/*
 			if (window.AudioPlugin !== undefined)
 			{
 				console.log("Did enter resources view");
-				ProfileService.getCurrentProfile().then( function(res) {
+				ProfileService.getCurrentProfile().then( function(res)
+				{
 					console.log("Got current user profile");
-					if (res && res.lpcOrder) {
+					if (res && res.lpcOrder)
+					{
 						$scope.currentProfileName = res.name;
 						$scope.data.lpcOrder = res.lpcOrder;
 						AudioPlugin.setLPCOrder($scope.data.lpcOrder, $scope.logPluginLPCOrder);
@@ -36,18 +39,25 @@
 						$scope.resetPluginLPCOrder();
 					}
 				});
+
 			};
+			*/
 		});
 
+		/*
 		$scope.configureLPC = function() {
 			$scope.data.configuring = true;
 		}
+		*/
 
+		/*
 		$scope.logPluginLPCOrder = function(order)
 		{
 			console.log("Plugin LPC order is now: " + order);
 		};
+		*/
 
+		/*
 		$scope.resetPluginLPCOrder = function() {
 			ProfileService.getCurrentProfile().then(function(res) {
 				if (res) {
@@ -58,12 +68,16 @@
 				$scope.updatePluginLPCOrder();
 			});
 		};
+		*/
 
+		/*
 		$scope.setLPCOrder = function(order)
 		{
 			$scope.data.lpcOrder = order;
 		};
+		*/
 
+		/*
 		$scope.updatePluginLPCOrder = function() {
 			if (window.AudioPlugin !== undefined) {
         ProfileService.runTransactionForCurrentProfile(function(handle, doc, t) {
@@ -72,6 +86,7 @@
         });
 			}
 		};
+		*/
 
 
 	});
