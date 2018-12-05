c0afa095975fb60dc42b9ebc46c084827337fc3c
helencarey <helencarey@nyu.edu>
2018-12-05 12:45:47 -0500

    Resources ready for PR

--- staRt/www/states/root/resources/resources_controller.js
+++ staRt/www/states/root/resources/resources_controller.js
@@ -6,10 +6,8 @@
 
 	resources.controller('ResourcesController', function($scope, $timeout, $localForage, ProfileService, StartUIState, $rootScope, $state)
 	{
-		console.log('ResourcesController here!');
+		//console.log('ResourcesController here!');
 
-		// configuring: false,
-		// lpcOrder: 35,
 		$scope.data = {
       version: "",
       platform: "",
@@ -23,71 +21,7 @@
 
 		$scope.$on("$ionicView.enter", function() {
 			console.log('resources content loaded!');
-			/*
-			if (window.AudioPlugin !== undefined)
-			{
-				console.log("Did enter resources view");
-				ProfileService.getCurrentProfile().then( function(res)
-				{
-					console.log("Got current user profile");
-					if (res && res.lpcOrder)
-					{
-						$scope.currentProfileName = res.name;
-						$scope.data.lpcOrder = res.lpcOrder;
-						AudioPlugin.setLPCOrder($scope.data.lpcOrder, $scope.logPluginLPCOrder);
-					} else {
-						$scope.resetPluginLPCOrder();
-					}
-				});
-
-			};
-			*/
 		});
-
-		/*
-		$scope.configureLPC = function() {
-			$scope.data.configuring = true;
-		}
-		*/
-
-		/*
-		$scope.logPluginLPCOrder = function(order)
-		{
-			console.log("Plugin LPC order is now: " + order);
-		};
-		*/
-
-		/*
-		$scope.resetPluginLPCOrder = function() {
-			ProfileService.getCurrentProfile().then(function(res) {
-				if (res) {
-					$scope.data.lpcOrder = ProfileService.lookupDefaultFilterOrder(res);
-				} else {
-					$scope.data.lpcOrder = 35;
-				}
-				$scope.updatePluginLPCOrder();
-			});
-		};
-		*/
-
-		/*
-		$scope.setLPCOrder = function(order)
-		{
-			$scope.data.lpcOrder = order;
-		};
-		*/
-
-		/*
-		$scope.updatePluginLPCOrder = function() {
-			if (window.AudioPlugin !== undefined) {
-        ProfileService.runTransactionForCurrentProfile(function(handle, doc, t) {
-          AudioPlugin.setLPCOrder($scope.data.lpcOrder, $scope.logPluginLPCOrder);
-          t.update(handle, {lpcOrder: $scope.data.lpcOrder});
-        });
-			}
-		};
-		*/
-
 
 	});
 
